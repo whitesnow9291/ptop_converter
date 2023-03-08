77bc2a4798fea0a67316a78b3c02e7e27c1a0767
g <g@gs-iMac.local>
2023-03-08 23:15:58 +0900

    updated

--- convert.py
+++ convert.py
@@ -1,16 +1,21 @@
 from lib2to3.pytree import convert
 import docx2txt
-from os import listdir, mkdir
+from os import listdir, mkdir, rmdir
+import shutil
 from os.path import isfile, join
 import tinify
 import json
 tinypng_api_key = 'cr8J4CzvN0W1QcyRXHVb6vxLjMb49fTl'
-directory = r"../chords"
+directory = r"./chords"
 destionation = r'./output'
 
 tinify.key = tinypng_api_key
 
 def convertDirectory(dir_path):
+    # init 
+    shutil.rmtree(destionation, ignore_errors=True)
+    mkdir(destionation)
+    # start
     files = listdir(directory)
     file_id = 0
     chord_data = []
@@ -25,7 +30,7 @@ def convertDirectory(dir_path):
         images = listdir(destPath)
         require_images = []
         for img in images:
-            img = f'require("../assets/chords/{file_id}/{img}")'
+            img = f"require('../assets/chords/{file_id}/{img}')"
             require_images.append(img)
         chord = {
             'id': file_id,
